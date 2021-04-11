1ad60f218c968c57290a10f68918678a113dedc4
Arvind-puthucode <62462676+Arvind-puthucode@users.noreply.github.com>
2021-04-11 14:04:27 +0530

    Update LibFS.cpp

--- LibFS.cpp
+++ LibFS.cpp
@@ -29,23 +29,23 @@ struct inode inode_arr[NO_OF_INODES];
 struct dir_entry
 {
     char file_name[16]; //filename size is 15 max
-    int inode_num;  // of this 
+    int inode_num;  // of this
 };
 struct dir_entry dir_struct_entry[NO_OF_INODES];
 
 struct super_block  // map of directory  structure
 {
-    
+
     /* directory information */
-    int firstDbOfDir = ceil( ((float)sizeof(super_block))/BLOCK_SIZE );   
+    int firstDbOfDir = ceil( ((float)sizeof(super_block))/BLOCK_SIZE );
     int noOfDbUsedinDir = (sizeof(struct dir_entry)*NO_OF_INODES)/BLOCK_SIZE;
     /* data information */
-    int startingInodesDb = firstDbOfDir+noOfDbUsedinDir; 
+    int startingInodesDb = firstDbOfDir+noOfDbUsedinDir;
     int total_Usable_DB_Used_for_inodes= ceil( ((float)(NO_OF_INODES * sizeof(struct inode))) / BLOCK_SIZE );
-    int starting_DB_Index= startingInodesDb + total_Usable_DB_Used_for_inodes;  
-    int total_Usable_DB= DISK_BLOCKS - starting_DB_Index; 
-    char inode_Bitmap[NO_OF_INODES]; 
-    char datablock_Bitmap[DISK_BLOCKS]; 
+    int starting_DB_Index= startingInodesDb + total_Usable_DB_Used_for_inodes;
+    int total_Usable_DB= DISK_BLOCKS - starting_DB_Index;
+    char inode_Bitmap[NO_OF_INODES];
+    char datablock_Bitmap[DISK_BLOCKS];
 };
 
 struct super_block sup_block;
@@ -78,7 +78,7 @@ int FS_Sync()
     printf("FS_Sync\n");
 
     fp = fopen(disk_name,"rb+");
-    //retrieve super block from virtual disk and store into global struct super_block sup_block 
+    //retrieve super block from virtual disk and store into global struct super_block sup_block
     char sup_block_buff[sizeof(sup_block)];
     memset(sup_block_buff,0,sizeof(sup_block));
     fread(sup_block_buff,1,sizeof(sup_block),fp);
@@ -140,8 +140,8 @@ void disk_Set()
     for (int i = 0; i < DISK_BLOCKS; ++i)
         fwrite(buffer,1,BLOCK_SIZE,fp);
         //write(fp, buffer, BLOCK_SIZE);
-    
-    
+
+
     struct super_block sup_block;  //initializing sup_block
 
     //setting DB vecor array to 1(=used i.e cant assign to file) for all metadata DB and 0(=free) to all data DBs
@@ -190,7 +190,7 @@ void disk_Set()
 
     fclose(fp);
     printf("Virtual Disk %s created sucessfully\n",disk_name );
-    
+
 }
 
 
@@ -199,9 +199,9 @@ void disk_Set()
 *******************************************************************/
 
 
-int 
+int
 FS_Boot(char *path)
-{   
+{
     //disk_name = path;
     strcpy(disk_name, path);
     cout<<"disk name:" << disk_name<<endl;
@@ -211,8 +211,8 @@ FS_Boot(char *path)
     if( access( disk_name, F_OK ) != -1 )
     {
         printf("The Disk already exists\n");
-    } 
-    else 
+    }
+    else
     {
         // oops, check for errors
         if (Disk_Init() == -1) {
@@ -251,28 +251,28 @@ int File_Create(char *filename)
     {
         printf("Create File Error : No more files can be created.Max number of files reached.\n");
         return -1;
-    }   
+    }
 
-    if(free_Db_list.size() == 0) 
+    if(free_Db_list.size() == 0)
     {
         printf("Error : Memory is fulled.\n");
         return -1;
     }
-    
+
     nextin = free_Inode_list.back();
     free_Inode_list.pop_back();
     nextdb = free_Db_list.back();
     free_Db_list.pop_back();
     inode_arr[nextin].pointer[0]=nextdb;
-    
-    
-    inode_arr[nextin].filesize = 0;  
-    
+
+
+    inode_arr[nextin].filesize = 0;
+
     directory_map[string(filename)] = nextin;
-    
+
     strcpy(dir_struct_entry[nextin].file_name,filename);
     dir_struct_entry[nextin].inode_num = nextin;
-    
+
     printf(" Voila!! File %s Created.\n",filename );
 
     return 0;
@@ -289,12 +289,12 @@ int File_Open(char *name)
     printf("FS_Open\n");
 
     int inode_no;
-    if ( directory_map.find(name) == directory_map.end() ) 
+    if ( directory_map.find(name) == directory_map.end() )
     {
         printf("Open File Error : File Not Found\n");
-        return -1;  
+        return -1;
     }
-    else 
+    else
     {
         if(free_FD_list.size() == 0)
         {
@@ -352,15 +352,15 @@ File_Read(int fildes, void  *buf, int nbyte)
                 break;
             }
             int block=inode_arr[inode].pointer[i];
-            
+
             read_block(block,read_buf);
-            
+
 
             if((tot_block-noOfBlocks >= FS/BLOCK_SIZE) && (noOfBlocks > 1))
             {
                 if(flag==0)
                 {
-                    fwrite(read_buf+(FS%BLOCK_SIZE),1,(BLOCK_SIZE-FS%BLOCK_SIZE),fp1);  
+                    fwrite(read_buf+(FS%BLOCK_SIZE),1,(BLOCK_SIZE-FS%BLOCK_SIZE),fp1);
                     flag=1;
                 }
                 else
@@ -394,57 +394,57 @@ int
 File_Write(int fd, char *buf, int size)
 {
 
-    
+
     //take user filename input in global var filename[20] (already declared) and size of file in a local var write_file_size (why local coz it'll get updated in inode at the end of function)
     //check if avail DB size > write_file_size else throw error & return
 
     int cur_pos=fileDescriptor_map[fd].second, db_to_write;
 
-    if( cur_pos%BLOCK_SIZE == 0 && size > BLOCK_SIZE ) 
+    if( cur_pos%BLOCK_SIZE == 0 && size > BLOCK_SIZE )
     {
         printf("more than 1 DB size needed\n");
         return -1;
     }
-    else if( (BLOCK_SIZE - cur_pos%BLOCK_SIZE) < size ) 
+    else if( (BLOCK_SIZE - cur_pos%BLOCK_SIZE) < size )
     {
         printf("size of data to write is more than remaining empty size of DB\n");
     }
 
-    int total_Usable_DB_used_till_cur_pos = cur_pos / BLOCK_SIZE ;  
+    int total_Usable_DB_used_till_cur_pos = cur_pos / BLOCK_SIZE ;
 
-    
-        if(inode_arr[fileDescriptor_map[fd].first].filesize == 0) 
+
+        if(inode_arr[fileDescriptor_map[fd].first].filesize == 0)
         {
             block_write(inode_arr[fileDescriptor_map[fd].first].pointer[0], buf, size, 0 );
-            inode_arr[fileDescriptor_map[fd].first].filesize += size; 
-            fileDescriptor_map[fd].second += size;  
+            inode_arr[fileDescriptor_map[fd].first].filesize += size;
+            fileDescriptor_map[fd].second += size;
         }
         else //if filesize is not 0
         {
-            if(cur_pos<inode_arr[fileDescriptor_map[fd].first].filesize) 
+            if(cur_pos<inode_arr[fileDescriptor_map[fd].first].filesize)
             {
                 if(total_Usable_DB_used_till_cur_pos < 10 )
                 {
                     db_to_write  = inode_arr[fileDescriptor_map[fd].first].pointer[total_Usable_DB_used_till_cur_pos];
-                    
+
                     block_write(db_to_write, buf, size, 0 );
 
-                    if( inode_arr[fileDescriptor_map[fd].first].filesize - cur_pos < size )  
+                    if( inode_arr[fileDescriptor_map[fd].first].filesize - cur_pos < size )
                         inode_arr[fileDescriptor_map[fd].first].filesize += size - (inode_arr[fileDescriptor_map[fd].first].filesize - cur_pos);
-                    fileDescriptor_map[fd].second += size;  
+                    fileDescriptor_map[fd].second += size;
                 }
-                else if(total_Usable_DB_used_till_cur_pos < 1034 ) 
+                else if(total_Usable_DB_used_till_cur_pos < 1034 )
                 {
                     int block = inode_arr[fileDescriptor_map[fd].first].pointer[10];
                     int blockPointers[1024];        //Contains the array of data block pointers.
                     char read_buf[(1<<15)];
                     read_block(block,read_buf);  //reading the block into read_buf
                     memcpy(blockPointers,read_buf,sizeof(read_buf));
-                    
+
 
 
                     db_to_write  = blockPointers[total_Usable_DB_used_till_cur_pos-10];
-                    
+
                     block_write(db_to_write, buf, size, 0);
 
                     if( inode_arr[fileDescriptor_map[fd].first].filesize - cur_pos < size )  //updating the filesize
@@ -459,7 +459,7 @@ File_Write(int fd, char *buf, int size)
                     char read_buf[(1<<15)];
                     read_block(block,read_buf);  //reading the block into read_buf
                     memcpy(blockPointers,read_buf,sizeof(read_buf));
-                    
+
                     int block2 = blockPointers[(total_Usable_DB_used_till_cur_pos-1034)/1024];
                     int blockPointers2[1024];       //Contains the array of data block pointers.
                     read_block(block2,read_buf);  //reading the block2 into read_buf
@@ -488,24 +488,69 @@ File_Write(int fd, char *buf, int size)
                     inode_arr[fileDescriptor_map[fd].first].filesize += size;
                     fileDescriptor_map[fd].second += size;  //updating the cur pos in the file
                 }
-                
-                
+
+
 
             }
         }
-    
+
 }
 
 int
 File_Seek(int fd, int offset)
 {
-    printf("FS_Seek\n");
+    printf("File_Seek\n");
+    if(offset < 0){
+        printf("File Seek: Negative Offset.\n");
+        osErrno = E_SEEK_OUT_OF_BOUNDS;
+        return -1;
+    }
+    else if(get_file_size(fd) == -1){
+        printf("File Seek: File Not Open.\n");
+        osErrno = E_BAD_FD;
+        return -1;
+    }
+    else if(get_file_size(fd) < offset){
+        printf("File Seek: Seek Out of Bound.\n");
+        osErrno = E_SEEK_OUT_OF_BOUNDS;
+        return -1;
     return 0;
+    }
 }
-
 int
 File_Close(char* name)
-{
+{   int i, table_sector, offset;
+    char buff[SECTOR_SIZE];
+
+    if(fd < 64){
+        table_sector = 4;
+        offset = (fd*8);
+    }
+    else if(fd > 63 && fd < 128){
+        table_sector = 5;
+        offset = (fd*8) - 64;
+    }
+    else if(fd > 127 && fd < 192){
+        table_sector = 6;
+        offset = (fd*8) - 128;
+    }
+    else if(fd > 191 && fd < 256){
+        table_sector = 7;
+        offset = (fd*8) - 192;
+    }
+
+    Disk_Read(table_sector, buff);
+    if(buff[offset] == 0){
+        printf("File Close: File Not Open\n");
+        osErrno = E_BAD_FD;
+        return -1;
+    }
+
+    for(i = offset; i < 8; i++){
+        buff[i] = '\0';
+    }
+    Disk_Write(table_sector, buff);
+    FS_Sync();
     return 0;
 }
 
@@ -567,22 +612,22 @@ int read_block(int block,char* buf)
   return 0;
 }
 
-int block_write(int block, char *buf, int size_to_wrt, int start_pos_to_wrt) 
-{                                                                           
+int block_write(int block, char *buf, int size_to_wrt, int start_pos_to_wrt)
+{
     cout<<"block_number:"<<block<<endl;
-  if ((block < 0) || (block >= DISK_BLOCKS)) 
+  if ((block < 0) || (block >= DISK_BLOCKS))
   {
     printf("Block write Error : block index out of bounds\n");
     return -1;
   }
 
-  if (fseek(fp, (block * BLOCK_SIZE)+start_pos_to_wrt, SEEK_SET) < 0) 
+  if (fseek(fp, (block * BLOCK_SIZE)+start_pos_to_wrt, SEEK_SET) < 0)
   {
     printf("Block write Error : failed to lseek");
     return -1;
   }
 
-  if (fwrite(buf, 1, size_to_wrt,fp) < 0) 
+  if (fwrite(buf, 1, size_to_wrt,fp) < 0)
   {
     perror("block_write: failed to write");
     return -1;
@@ -622,12 +667,12 @@ int store_file_into_Disk(char * filename)
         return -1;
     }
 
-    
+
 
     remain_size_OfLast_db=BLOCK_SIZE;
-    
-    
-    if( remain_size_OfLast_db >= fp2_filesize ) 
+
+
+    if( remain_size_OfLast_db >= fp2_filesize )
     {
         fread(buf2, fp2_filesize, 1, fp2);
         buf2[fp2_filesize] = '\0';
@@ -635,22 +680,22 @@ int store_file_into_Disk(char * filename)
     }
     else
     {
-        
+
         fread(buf2, remain_size_OfLast_db, 1, fp2);
         buf2[remain_size_OfLast_db] = '\0';
         File_Write(fd2, buf2, remain_size_OfLast_db);
-        
+
         int remaining_block_count = (fp2_filesize - remain_size_OfLast_db) / BLOCK_SIZE;
         while(remaining_block_count--)
         {
             fread(buf2, BLOCK_SIZE, 1, fp2);
             buf2[BLOCK_SIZE] = '\0';
             File_Write(fd2, buf2, BLOCK_SIZE);
-           
+
         }
-        
+
         int remaining_size = (fp2_filesize - remain_size_OfLast_db) % BLOCK_SIZE;
-        
+
         fread(buf2, remaining_size, 1, fp2);
         buf2[remaining_size] = '\0';
         File_Write(fd2, buf2, remaining_size);
@@ -658,11 +703,11 @@ int store_file_into_Disk(char * filename)
     }
 
    // close_file(fd2);
-    if ( fileDescriptor_map.find(fd2) == fileDescriptor_map.end() ) 
+    if ( fileDescriptor_map.find(fd2) == fileDescriptor_map.end() )
     {
         printf("File Descripter %d not found\n",fd2);
     }
-    else 
+    else
     {
         fileDescriptor_map.erase(fd2);
         openfile_count--;
@@ -677,7 +722,7 @@ int store_file_into_Disk(char * filename)
 
 int unmounting()
 {
-   
+
     //storing sup_block into begining of virtual disk
     fseek( fp, 0, SEEK_SET );
     char sup_block_buff[sizeof(struct super_block)];
@@ -703,7 +748,7 @@ int unmounting()
     memcpy(inode_buff,inode_arr,sizeof(inode_arr));
     fwrite(inode_buff,1,sizeof(inode_arr),fp);
 
-    
+
 
     printf("Disk Unmounted\n");
     fclose(fp);
